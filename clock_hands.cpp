44a0c640c2c904889c2019961e89aefc13580d94
Rich Nakasato <richnakasato@hotmail.com>
2019-01-25 13:17:07 -0500

    example from coderpad (cruise?)

---
+++ clock_hands.cpp
@@ -0,0 +1,22 @@
+/*
+
+A standard circular wall clock has three hands (hours, minutes, seconds).
+When do all three hands line up? Write a program to answer this question.
+Your program should print the times of day (hh:mm:ss) when the 3 hands line up.
+
+Assumptions:
+    1. There are 60 evenly spaced ticks on the clock face.
+    2. On each second:
+        - The second hand rotates by one tick
+        - The minute hand rotates by a fraction of a tick
+        - The hour hand rotates by an even smaller fraction of tick
+    3. Hands moves discretely and instantaneously.
+       For example, the second hand will never be between two ticks.
+    4. Hands are "lined up" when they are all within a full tick of each other.
+
+ */
+
+int main()
+{
+    return 0;
+}
