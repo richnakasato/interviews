28f98912400aadbb3df481bc04ccba2d3b40bb67
Rich Nakasato <richnakasato@hotmail.com>
2019-04-05 18:03:05 -0400

    implemented iterative version

--- binary_permutations.py
+++ binary_permutations.py
@@ -9,16 +9,34 @@ def helper(arr, n, curr, results):
         return
 
 
-def binary_permutations(arr, n):
+def binary_perm_recurse(arr, n):
     results = list()
     helper(arr, n, '', results)
     return results
 
 
+def binary_perm_iter(arr, n):
+    count = 2**n
+    res = arr
+    n -= 1
+    while n:
+        left = res[:]
+        right = res[:]
+        for i in range(len(res)):
+            left[i] = 'A'+left[i]
+            right[i] = 'B'+right[i]
+        res = left + right
+        n-=1
+    return res
+
+
 def main():
     arr = ['A','B']
-    result = binary_permutations(arr, 4)
-    assert result==["AAAA", "AAAB", "AABA", "AABB", "ABAA", "ABAB", "ABBA", "ABBB", "BAAA", "BAAB", "BABA", "BABB", "BBAA", "BBAB", "BBBA", "BBBB"]
+    n = 15
+    for i in range(1,n+1):
+        recursive = binary_perm_recurse(arr, i)
+        iterative = binary_perm_iter(arr, i)
+        assert recursive==iterative
 
 
 if __name__ == "__main__":
